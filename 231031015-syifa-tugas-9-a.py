f16e87e3dac7dbe0a049c455826a1a443d084524
syifatriagusriani <154118734+syifatriagusriani@users.noreply.github.com>
2023-12-23 11:49:24 +0800

    Add files via upload

---
+++ 231031015-syifa-tugas-9-a.py
@@ -0,0 +1,27 @@
+data= {'nama lengkap'        : 'Syifa Tri Agusriani',
+       'nama panggilan '     : 'Syifa',
+       'NIM'                 : '231031015',
+       'tempat/tanggal lahir': 'Parepare, 13 Agustus 2005',
+       'agama'               : 'Islam',
+       'umur'                : '18 Tahun',
+       'jenis kelamin'       : 'Perempuan',
+       'tinggi badan'        : '158 cm',
+       'berat badan'         : '59 kg',
+       'alamat'              : 'Jl.Jend.Muh.Yusuf',
+       'kelurahan'           : 'Lemoe',
+       'kecamatan'           : 'Bacukiki',
+       'kota'                : 'Parepare',
+       'nomor hp'            : '082226684310',
+       'PTN'                 : 'Institut Teknologi Bacharuddin Jusuf Habibie',
+       'program studi'       : 'Sistem Informasi',
+       'angkatan'            : '2023'
+       }
+
+print('Nama                 :', data['nama lengkap'])
+print('NIM                  :', data['NIM'])
+print('Tempat/Tanggal Lahir :', data['tempat/tanggal lahir'])
+print('Jenis kelamin        :', data['jenis kelamin'])
+print('Agama                :', data['agama'])
+print('PTN                  :', data['PTN'])
+print('Program Studi        :', data['program studi'])
+print('Angkatan             :', data['angkatan'])
